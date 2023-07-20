b9e642d258dfd39bc727b010c6a56ca02c814d42
Sujeong Ji <suji.sujeongji@gmail.com>
2023-07-21 00:34:37 +0900

    feat: dynamic API routes and DELETE request

---
+++ 05-next-api-routes/pages/api/comments/[commentId].js
@@ -0,0 +1,24 @@
+import { comments } from "../../../data/comments";
+
+export default function handler(req, res) {
+  const { commentId } = req.query;
+
+  if (req.method === "GET") {
+    res.status(200).json(comments);
+  } else if (req.method === "DELETE") {
+    // find the comment to delete
+    const deleteComment = comments.find(
+      (comment) => comment.id === parseInt(commentId)
+    );
+
+    // find the index of the comment to delete
+    const index = comments.findIndex(
+      (comment) => comment.id === parseInt(commentId)
+    );
+    // remove the comment from array
+    comments.splice(index, 1);
+
+    // send back the deleted comment
+    res.status(200).json(deleteComment);
+  }
+}
--- 05-next-api-routes/pages/comments/index.js
+++ 05-next-api-routes/pages/comments/index.js
@@ -25,6 +25,16 @@ const CommentsPage = () => {
     setComment("");
   };
 
+  const deleteComment = async (commentId) => {
+    const response = await fetch(`/api/comments/${commentId}`, {
+      method: "DELETE",
+    });
+
+    const data = await response.json();
+    console.log(data);
+    fetchComments();
+  };
+
   return (
     <>
       <input
@@ -35,7 +45,12 @@ const CommentsPage = () => {
       <button onClick={submitComment}>Submit comment</button>
       <button onClick={fetchComments}>Load comments</button>
       {comments.map((comment) => {
-        return <div key={comment.id}>{comment.text}</div>;
+        return (
+          <div key={comment.id}>
+            {comment.text}
+            <button onClick={() => deleteComment(comment.id)}>Delete</button>
+          </div>
+        );
       })}
     </>
   );
