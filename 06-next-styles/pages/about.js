fd3f3b67509c9b646c5fe8271c4d1eb77890d461
Sujeong Ji <suji.sujeongji@gmail.com>
2023-07-21 12:18:41 +0900

    feat: SASS support

--- 06-next-styles/pages/about.js
+++ 06-next-styles/pages/about.js
@@ -1,9 +1,11 @@
-import styles from "../styles/About.module.css";
+// import styles from "../styles/About.module.css";
+import styles from "../styles/About.module.scss";
 
 const About = () => {
   return (
     <>
-      <div className={styles.highlight}>About Page</div>
+      {/* <div className={styles.highlight}>About Page</div> */}
+      <div className={styles.highlightscss}>About Page</div>
       <h2>About</h2>
       <button className="btn btn-primary">Click me</button>
       <button className="btn btn-success">Click me</button>
--- 06-next-styles/pages/contact.js
+++ 06-next-styles/pages/contact.js
@@ -1,9 +1,11 @@
-import styles from "../styles/Contact.module.css";
+// import styles from "../styles/Contact.module.css";
+import styles from "../styles/Contact.module.scss";
 
 const Contact = () => {
   return (
     <>
-      <div className={styles.highlight}>Contact Page</div>
+      {/* <div className={styles.highlight}>Contact Page</div> */}
+      <div className={styles.highlightscss}>Contact Page</div>
       <h2>Contact</h2>;
     </>
   );
