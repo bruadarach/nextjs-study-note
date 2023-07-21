d5a38b477734c954419795b2c8fce2bee645155c
Sujeong Ji <suji.sujeongji@gmail.com>
2023-07-21 11:42:50 +0900

    feat: global styles

---
+++ 06-next-styles/pages/about.js
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <>
+      <h2>About</h2>
+      <button className="btn btn-primary">Click me</button>
+      <button className="btn btn-success">Click me</button>
+      <button className="btn btn-secondary">Click me</button>
+    </>
+  );
+};
+
+export default About;
