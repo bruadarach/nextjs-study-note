20d4c817d4b6fa3cc178279219ba8ad124efe0b4
Sujeong Ji <suji.sujeongji@gmail.com>
2023-08-04 23:52:01 +0900

    feat: server-side authentication

--- 08-next-auth/pages/blog.js
+++ 08-next-auth/pages/blog.js
@@ -1,5 +1,20 @@
-const Blog = () => {
-  return <div>Blog</div>;
+import { getSession } from "next-auth/client";
+
+const Blog = ({ data }) => {
+  return <div>Blog - {data}</div>;
 };
 
 export default Blog;
+
+export async function getServerSideProps(context) {
+  // it returns a promise
+  const session = await getSession(context);
+
+  return {
+    props: {
+      data: session
+        ? "List of 100 personalized blog posts"
+        : "List of free blog posts",
+    },
+  };
+}
