cc2607870e44cb11c9172bb479fc4b486914c2f2
Sujeong Ji <suji.sujeongji@gmail.com>
2023-08-10 23:57:26 +0900

    feat: callbacks

--- 08-next-auth/pages/api/auth/[...nextauth].js
+++ 08-next-auth/pages/api/auth/[...nextauth].js
@@ -2,6 +2,14 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongodb";
+import { getToken } from "next-auth/jwt";
+import { getToken } from "next-auth/jwt";
+
+// 토큰을 가져오는 함수
+const getTokenForUser = async () => {
+  const token = await getToken({ req, secret: process.env.JWT_SECRET });
+  return token;
+};
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -13,6 +21,21 @@ export const authOptions = {
   ],
   adapter: MongoDBAdapter(clientPromise), // MongoDB 어댑터
   secret: process.env.JWT_SECRET, // JWT를 위한 비밀 키
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.accessToken = user.accessToken;
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      session.user.id = token.id;
+      return session;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
